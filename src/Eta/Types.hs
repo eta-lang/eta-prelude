7caa20b13bfc8faf22655175cfca72468816edfb
Nikita Tchayka <nikitatchayka@gmail.com>
2018-04-26 15:25:06 +0100

    Add ByteString

--- src/Eta/Types.hs
+++ src/Eta/Types.hs
@@ -12,6 +12,7 @@ where
 import Data.Int(Int, Int8, Int16, Int32, Int64)
 
 import Eta.Types.Bool as Exported
+import Eta.Types.ByteString as Exported hiding (pack, unpack)
 import Eta.Types.Char as Exported
 import Eta.Types.Either as Exported
 import Eta.Types.IO as Exported
---
+++ src/Eta/Types/ByteString.hs
@@ -0,0 +1,9 @@
+module Eta.Types.ByteString
+  ( ByteString(..)
+  , pack
+  , unpack
+  )
+where
+
+import Data.ByteString
+
